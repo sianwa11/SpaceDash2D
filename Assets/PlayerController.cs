938fa73d0a538800e0b54360c2a954b6d72a2c50
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2020-06-20 20:57:14 +0300

    obstacles and camera follow

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,15 +7,17 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     private void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+       /* if(Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
-        }
+        }*/
 
-        if (Input.GetKey(KeyCode.D))
+        rb.velocity = new Vector2(7, rb.velocity.y);
+
+        /*if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
-        }
+        }*/
 
         if(Input.GetKey(KeyCode.Space))
         {
