f7ff3b355cf3246bef715592b4caede86e176d52
sianwa11 <sianwa.atemi@strathmore.edu>
2020-06-20 23:20:13 +0300

    Collission detection

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D enemy)
+    {
+        if(enemy.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Collision Enter");
+
+            // play death sound and reduce life by one
+
+            // restart level if life is not exhausted
+
+            // Game over if lives are exhausted
+        }
+    }
+}
