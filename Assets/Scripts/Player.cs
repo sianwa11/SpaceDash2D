c3df16c4e9f0e3a71594301f2d885c888592f170
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2020-07-03 21:04:45 +0300

    Health Bar & Jumping

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth); // set maxHealth to 100
+    }
+
+    // Collission detection
+    private void OnCollisionEnter2D(Collision2D enemy)
+    {
+        if(enemy.gameObject.tag == "Enemy")
+        {
+            Debug.Log("HIT Enemy");
+
+            // play death sound and reduce life
+            takeDamage(20);
+
+            // restart level if life is not exhausted
+
+            // Game over if lives are exhausted
+        }
+    }
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Range(1,10)]
+    public float jumpVelocity;
+    public KeyCode jumpKey;
+    public bool grounded;
     public Rigidbody2D rb;
-    private void Update()
-    {
-       /* if(Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
-        }*/
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // to prevent double jumps
+        if (collision.gameObject.tag == "Foreground")
+        {
+            grounded = true;
+        }
+    }
+
+    void Update()
+    {
+        // later on fix this forward movement
         rb.velocity = new Vector2(7, rb.velocity.y);
 
-        /*if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(jumpKey) && grounded)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y);
-        }*/
-
-        if(Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 10);
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+            grounded = false;
         }
     }
 }
